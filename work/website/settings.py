81b25da661c6287b772ef64a8b9bd9d09ebf8ba0
kelvinhere <kwardeu@gmail.com>
2022-04-08 14:10:14 +0100

    DOCS: Deployment instructions added to README.MD, MINOR: Fix wrong enviromental variable file in docker-compose.yml

--- work/website/settings.py
+++ work/website/settings.py
@@ -83,11 +83,11 @@ WSGI_APPLICATION = 'website.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
-        "NAME": os.environ.get("SQL_DATABASE"),
-        "USER": os.environ.get("SQL_USER"),
-        "PASSWORD": os.environ.get("SQL_PASSWORD"),
-        "HOST": os.environ.get("SQL_HOST"),
-        "PORT": os.environ.get("SQL_PORT"),
+        "NAME": os.environ.get("SQL_DATABASE", os.path.join(BASE_DIR, "db.sqlite3")),
+        "USER": os.environ.get("SQL_USER", ""),
+        "PASSWORD": os.environ.get("SQL_PASSWORD", ""),
+        "HOST": os.environ.get("SQL_HOST", ""),
+        "PORT": os.environ.get("SQL_PORT", ""),
     }
 }
 
